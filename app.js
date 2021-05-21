0527e66f7c5adaf7893f89593986d5ef0933aa26
quang doan <doan_q@qwasar.io>
2021-05-21 02:00:08 +0000

    added comment documentation about the endpoints

--- app.js
+++ app.js
@@ -29,44 +29,84 @@ const sinatra_songs = [
     "Say It", 
     "The Sea Song", 
     "The Second Time Around"
-    ];
+];
 
-const sinatra_wives = ["Nancy Barbato", "Ava Gardner", "Mia Farrow", "Barbara Marx"];
+const sinatra_wives = [
+    "Nancy Barbato", 
+    "Ava Gardner", 
+    "Mia Farrow", 
+    "Barbara Marx"
+];
 
+
+/**
+ * Object containing Frank Sinatra's information
+ */
 const Sinatra = {
     birth_date : "December 12, 1915",
     birth_city : "Hoboken, New Jersey, U.S.",
     wives : sinatra_wives,
     songs : sinatra_songs
 }
-
+/**
+ * Returns a random song by Frank Sinatra from a list of 20 songs.
+ */
 app.get('/', (req, res) => {
     var randomIndex = Math.floor((Math.random() * sinatra_songs.length));
     var song = Sinatra.songs[randomIndex];
   res.send(song);
 });
 
+/**
+ * Returns the birth date of Frank Sinatra
+ */
 app.get('/birth_date', (req, res) => {
     res.send(Sinatra.birth_date);
 });
 
+
+/**
+ * Returns the birth place where Frank Sinatra was born
+ */
 app.get('/birth_city', (req, res) => {
     res.send(Sinatra.birth_city);
 });
 
+/**
+ * Returns all of Frank Sinatra's wives has was married to
+ */
 app.get('/wives', (req, res) => {
     res.send(Sinatra.wives.join(', '));
 });
 
+/**
+ * Returns a response that anybody can reach without having certain permissions
+ */
 app.get('/public', (req, res) => {
     res.send(server_responses.public);
 });
 
+
+/**
+ * Basic authorization with access using credentials in the URL and 
+ * accessing the protected endpoint
+ * 
+ * scheme - username:password@
+ * the scheme is added to front of url 
+ * e.g. username:password@web-XXXX.docode.qwasar.io
+ * 
+ * Returns an unauthorized response if url does not have 
+ * correct credentials passed into url using the protected endpoint
+ */
 app.use(basicAuth({
     users: { 'admin': 'admin' },
     unauthorizedResponse : `${server_responses.unauthorized}`
 }))
 
+/**
+ * Returns a server response for authorized when the correct 
+ * credentials are passed into url using the protected endpoint
+ */
 app.get('/protected', (req, res) => {
     console.log("credentials", req.auth);
     res.send(server_responses.protected);
